ce85638ef5031da2f89076ba27bcd1181df61902
craigsdennis <craig@teamtreehouse.com>
2016-07-18 23:29:57 -0400

    Moves to API based approach

--- rollup.config.js
+++
@@ -1,8 +0,0 @@
-import babel from 'rollup-plugin-babel';
-
-export default {
-  entry: 'examples/main.js',
-  format: 'iife',
-  plugins: [ babel() ],
-  dest: 'bundle.js'
-};
---
+++ src/prepare.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const rollup = require('rollup').rollup;
+const babel = require('rollup-plugin-babel');
+
+rollup({
+    entry: 'examples/main.js',
+    plugins: [
+        babel({
+            presets: ['es2015-rollup']
+        })
+    ]
+}).then(bundle => {
+    let result = bundle.generate({
+        format: 'iife'
+    });
+    console.log('Got back');
+    console.log(result.code);
+}).catch(err => console.error(err));
