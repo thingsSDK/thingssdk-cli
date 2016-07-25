c7d0c1233ccb8011eaf393b463ea9d39c052b5dc
Andrew Chalkley <andrew@chalkley.org>
2016-07-24 22:55:01 -0700

    Wrong package name

--- templates/espruino/scripts/push.js
+++ templates/espruino/scripts/push.js
@@ -1,11 +1,11 @@
 'use strict';
 const deployer = require('thingssdk-deployer')();
 
-const espruino = require('thingssdk-espruino');
+const espruinoStartegy = require('thingssdk-espruino-strategy');
 
 const iotPackageJson = require('../package.json');
 const devices = require('../devices.json').devices;
 
-deployer.build('espruino', espruino.build(iotPackageJson.main));
+deployer.build('espruino', espruinoStartegy.build(iotPackageJson.main));
 
-deployer.upload('espruino', espruino.upload(devices));
+deployer.upload('espruino', espruinoStartegy.upload(devices));
