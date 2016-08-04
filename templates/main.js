3bdf02573534eed51c68e5086054f363d891e5c2
Joseph Fraley <jose.fraley@gmail.com>
2016-08-04 11:13:40 -0700

    Updates the hello world to blink.

--- templates/main.js
+++ templates/main.js
@@ -1,1 +1,7 @@
-console.log('Hello, IoT!');
+var isOn = false;
+var interval = 500; // 500 milliseconds = 0.5 seconds
+
+setInterval(function(){
+    isOn = !isOn; // Flips the state on or off
+    digitalWrite(D2, isOn); // D2 is the blue LED on the ESP8266 boards
+}, interval);
