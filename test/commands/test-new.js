3728f33b4e8492e7135aba59db866e373eed6624
Joseph Fraley <jose.fraley@gmail.com>
2016-08-08 16:49:16 -0700

    Includes unit test placeholders for all commands/new functions.

--- test/commands/test-new.js
+++ test/commands/test-new.js
@@ -10,65 +10,83 @@ const {
   isDirectoryEmpty
 } = require('../../bin/commands/new.js')
 
-describe('function isDirectoryEmpty()', () => {
-  it('should throw an error if given no path', () => {
-    const runWithoutPath = () => isDirectoryEmpty()
-    expect(runWithoutPath).to.throw(Error);
-  })
+describe('thingssdk new', () => {
 
-  it('should return false if any files are present in the given directory', (done) => {
-    /**
-      An arbitrary directory that we know isn't empty
-    */
-    const thisDirectory = path.resolve(__dirname)
+  describe('function isDirectoryEmpty()', () => {
+    it('should throw an error if given no path', () => {
+      const runWithoutPath = () => isDirectoryEmpty()
+      expect(runWithoutPath).to.throw(Error);
+    })
 
-    /**
-      Initialize actual here so that we can update it with
-      the async results of isDirectoryEmpty
-    */
-    let actual
-    const expected = false
-
-    /**
-      asyncResult is passed to the callback via
-      isDirectoryEmpty internal logic
-    */
-    const callback = (asyncResult) => {
-      actual = asyncResult
-      expect(actual).to.equal(expected)
-      done()
-    }
-
-    isDirectoryEmpty(thisDirectory, callback)
-  })
-
-  it('should return true if the given directory is empty', (done) => {
-    const emptyDirectory = path.resolve(__dirname, 'empty')
-    mkdirp(emptyDirectory)
-
-    /**
-      Initialize actual here so that we can update it with
-      the async results of isDirectoryEmpty
-    */
-    let actual
-    const expected = true
-
-    /**
-      asyncResult is passed to the callback via
-      isDirectoryEmpty internal logic
-    */
-    const callback = (asyncResult) => {
-      actual = asyncResult
-      expect(actual).to.equal(expected)
+    it('should return false if any files are present in the given directory', (done) => {
+      /**
+        An arbitrary directory that we know isn't empty
+      */
+      const thisDirectory = path.resolve(__dirname)
 
       /**
-        Destroy the empty directory we made for this test
-        before we finish
+        Initialize actual here so that we can update it with
+        the async results of isDirectoryEmpty
       */
-      fs.rmdir(emptyDirectory)
-      done()
-    }
+      let actual
+      const expected = false
 
-    isDirectoryEmpty(emptyDirectory, callback)
+      /**
+        asyncResult is passed to the callback via
+        isDirectoryEmpty internal logic
+      */
+      const callback = (asyncResult) => {
+        actual = asyncResult
+        expect(actual).to.equal(expected)
+        done()
+      }
+
+      isDirectoryEmpty(thisDirectory, callback)
+    })
+
+    it('should return true if the given directory is empty', (done) => {
+      const emptyDirectory = path.resolve(__dirname, 'empty')
+      mkdirp(emptyDirectory)
+
+      /**
+        Initialize actual here so that we can update it with
+        the async results of isDirectoryEmpty
+      */
+      let actual
+      const expected = true
+
+      /**
+        isDirectoryEmpty internal logic
+        asyncResult is passed to the callback via
+      */
+      const callback = (asyncResult) => {
+        actual = asyncResult
+        expect(actual).to.equal(expected)
+
+        /**
+          Destroy the empty directory we made for this test
+          before we finish
+        */
+        fs.rmdir(emptyDirectory)
+        done()
+      }
+
+      isDirectoryEmpty(emptyDirectory, callback)
+    })
   })
+
+  /**
+    TODO:jmf continue unit tests.
+    Remove any placeholders below that are deemed unworthy of unit testing.
+  */
+  xdescribe('function createApplication()', () => {})
+  xdescribe('function applicationFinished()', () => {})
+  xdescribe('function createFiles()', () => {})
+  xdescribe('function copy()', () => {})
+  xdescribe('function write()', () => {})
+  xdescribe('function createPackageJSON()', () => {})
+  xdescribe('function clearLine()', () => {})
+  xdescribe('function printLoading()', () => {})
+  xdescribe('function getPorts()', () => {})
+  xdescribe('function createDevicesJSON()', () => {})
 })
