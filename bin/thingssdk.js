a6b57382dc7d33c7c41fe13945305db6a76b54d7
Andrew Chalkley <andrew@chalkley.org>
2016-08-08 19:21:39 -0700

    Updated readme for devices command

--- bin/thingssdk.js
+++ bin/thingssdk.js
@@ -6,6 +6,14 @@ const cliPackage = require("../package.json");
 const argv = require("yargs")
                 .version(cliPackage.version)
                 .commandDir('commands')
+                .option("baud_rate", {
+                    alias: "b",
+                    describe: "Baud rate for the device"
+                })
+                .option("port", {
+                    alias: "p",
+                    describe: "Port for the device"
+                })
                 .demand(1)
                 .strict()
                 .help()
