701491c16863c748154e367e142a9513c91f7af8
Matthew Curtis <matthew@matthewbryancurtis.com>
2016-10-27 19:04:10 -0700

    Add ES6 to main.js

--- templates/main.js
+++ templates/main.js
@@ -1,7 +1,7 @@
-var isOn = false;
-var interval = 500; // 500 milliseconds = 0.5 seconds
+let isOn = false;
+const interval = 500; // 500 milliseconds = 0.5 seconds
 
-setInterval(function(){
+setInterval(() => {
     isOn = !isOn; // Flips the state on or off
     digitalWrite(D2, isOn); // D2 is the blue LED on the ESP8266 boards
 }, interval);
