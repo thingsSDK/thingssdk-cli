355ecc26456800bb4a6db0266b5631d6a3849dc3
Andrew Chalkley <andrew@chalkley.org>
2017-06-01 21:37:31 -0700

    Fixing depednancies and updating tests

--- bin/thingssdk.js
+++ bin/thingssdk.js
@@ -6,13 +6,17 @@ const cliPackage = require("../package.json");
 const argv = require("yargs")
                 .version(cliPackage.version)
                 .commandDir('../lib/commands')
+                .option("runtime", {
+                    alias: "r",
+                    describe: "Runtime for the device",
+                })
                 .option("baud_rate", {
                     alias: "b",
                     describe: "Baud rate for the device"
                 })
                 .option("port", {
                     alias: "p",
-                    describe: "Port for the device"
+                    describe: "Serial port for the device"
                 })
                 .demand(1)
                 .strict()
